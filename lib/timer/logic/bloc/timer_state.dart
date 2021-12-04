923238b254f8f72f2b488d790121244bc8e00046
Breno Battaglin <breno.battaglin@icloud.com>
2021-12-04 12:47:29 -0300

    Create events and states

---
+++ lib/timer/logic/bloc/timer_state.dart
@@ -0,0 +1,38 @@
+part of 'timer_bloc.dart';
+
+abstract class TimerState extends Equatable {
+  final int duration;
+
+  const TimerState(this.duration);
+
+  @override
+  List<Object> get props => [duration];
+}
+
+class TimerInitial extends TimerState {
+  TimerInitial(int duration) : super(duration);
+
+  @override
+  String toString() => 'TimerInitial { duration: $duration }';
+}
+
+class TimerRunning extends TimerState {
+  TimerRunning(int duration) : super(duration);
+
+  @override
+  String toString() => 'TimerRunning { duration: $duration }';
+}
+
+class TimerRunningPaused extends TimerState {
+  TimerRunningPaused(int duration) : super(duration);
+
+  @override
+  String toString() => 'TimerRunningPaused { duration: $duration }';
+}
+
+class TimerRunningFinished extends TimerState {
+  TimerRunningFinished(int duration) : super(duration);
+
+  @override
+  String toString() => 'TimerRunningFinished { duration: $duration }';
+}
